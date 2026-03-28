d6d7173cbe1c1f3334bb09244925ac57e5225179
skye-tachyon <sonicgames772@gmail.com>
2026-03-28 17:40:45 +0000

    ak3: init zero(f)lte
    * Galaxy S6/Edge
    
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -10,14 +10,15 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=wisebl
+device.name1=zerolte
+device.name2=zeroflte
 '; } # end properties
 
 # boot shell variables
-BLOCK=auto;
-IS_SLOT_DEVICE=auto;
+BLOCK=/dev/block/platform/15570000.ufs/by-name/BOOT;
+IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
+PATCH_VBMETA_FLAG=0;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
