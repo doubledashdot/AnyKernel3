f6702a3be59615f5bb91d36e0976fe2b89fd09f7
@skye <sonicgames772@gmail.com>
2026-05-06 22:43:01 -0300

    ok sar

--- anykernel.sh
+++ anykernel.sh
@@ -13,8 +13,7 @@ do.cleanuponabort=0
 device.name1=r8q
 device.name2=r8qxx
 device.name3=r8qxxx
-device.name4=ossi
-supported.versions=11 - 16
+supported.versions=14 - 16
 supported.patchlevels=
 '; } # end properties
 
