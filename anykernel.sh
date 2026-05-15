4ec6b7fa7f52a01a7b2b2706d40891116867a770
@skye <sonicgames772@gmail.com>
2026-05-15 18:19:15 -0300

    anykernel.sh: init lineageksu

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=not_kernel by skye-tachyon @ xda-developers
+kernel.string=LineageKSU by skye-tachyon @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -34,44 +34,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 ## AnyKernel boot install
 
 split_boot;
-
-case "$ZIPFILE" in
-   *-eff*)
-    mv $home/kona-eff.dtb $home/dtb
-    ;;
-   *)
-    mv $home/kona.dtb $home/dtb
-    ;;
-esac
-
-# begin cmdline changes
-oneui=$(file_getprop /system/build.prop ro.build.version.oneui);
-cos=$(file_getprop /system/build.prop ro.product.system.brand);
-gos=$(file_getprop /system/build.prop ro.build.host);
-if [ "$oneui" = "80000" ]; then
-   patch_cmdline "android.is_aosp" "android.is_aosp=0";
-   patch_cmdline "android.is_uos" "android.is_ous=1";
-elif [ -n "$oneui" ]; then
-   patch_cmdline "android.is_aosp" "android.is_aosp=0";
-   patch_cmdline "android.is_uos" "android.is_ous=0";
-elif [ "$gos" = "tachyon" ]; then
-   patch_cmdline "androidboot.selinux" "androidboot.selinux=permissive";
-   patch_cmdline "android.is_aosp" "android.is_aosp=0";
-   patch_cmdline "android.is_uos" "android.is_ous=0";
-   patch_cmdline "ro.boot.verifiedbootstate=orange" "ro.boot.verifiedbootstate=green";
-   patch_cmdline "androidboot.verifiedbootstate=orange" "androidboot.verifiedbootstate=green";
-elif [ "$cos" = "oplus" ]; then
-   patch_cmdline "androidboot.selinux" "androidboot.selinux=permissive";
-   patch_cmdline "android.is_aosp" "android.is_aosp=1";
-   patch_cmdline "android.is_uos" "android.is_ous=0";
-else
-   patch_cmdline "ro.boot.verifiedbootstate=orange" "ro.boot.verifiedbootstate=green";
-   patch_cmdline "android.is_aosp" "android.is_aosp=1";
-   patch_cmdline "android.is_uos" "android.is_ous=0";
-fi
-
 dd if=$home/vbmeta.img of=/dev/block/platform/soc/1d84000.ufshc/by-name/vbmeta
 dd if=$home/dtbo.img of=/dev/block/platform/soc/1d84000.ufshc/by-name/dtbo
-
 flash_boot;
 ## end boot install
